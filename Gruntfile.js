a0a0ec73e628b51f5bb1befb2d0ab748e6fa3586
Minoru Kawamoto <minoru-github@k12u.org>
2015-03-20 19:29:28 +0900

    mine settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,62 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    nodewebkit: {
-      basecamp: {
-        options: {
-            build_dir: './webkitbuilds',
-            mac: true,
-            win: false,
-            linux32: false,
-            linux64: false,
-            mac_icns: './basecamp-src/icon.icns',
-            zip: true,
-            credits: './basecamp-src/credits.html'
-        },
-        src: ['./basecamp-src/**/*']
-      },
-      gmail: {
-        options: {
-            build_dir: './webkitbuilds',
-            mac: true,
-            win: false,
-            linux32: false,
-            linux64: false,
-            mac_icns: './gmail-src/icon.icns',
-            zip: true,
-            credits: './gmail-src/credits.html'
-        },
-        src: ['./gmail-src/**/*']
-      },
-      harvest: {
-        options: {
-            build_dir: './webkitbuilds',
-            mac: true,
-            win: false,
-            linux32: false,
-            linux64: false,
-            mac_icns: './harvest-src/icon.icns',
-            zip: true,
-            credits: './harvest-src/credits.html'
-        },
-        src: ['./harvest-src/**/*']
-      },
-      trello: {
-        options: {
-            build_dir: './webkitbuilds',
-            mac: true,
-            win: false,
-            linux32: false,
-            linux64: false,
-            mac_icns: './trello-src/icon.icns',
-            zip: true,
-            credits: './trello-src/credits.html'
-        },
-        src: ['./trello-src/**/*']
-      },
-      stripe: {
-        options: {
-            build_dir: './webkitbuilds',
-            mac: true,
-            win: false,
-            linux32: false,
-            linux64: false,
-            mac_icns: './stripe-src/icon.icns',
-            zip: true,
-            credits: './stripe-src/credits.html'
-        },
-        src: ['./stripe-src/**/*']
-      }
-    },
-  });
+	grunt.initConfig({
+		nodewebkit: {
+			gmail: {
+				options: {
+					build_dir: './webkitbuilds',
+					mac: true,
+					win: false,
+					linux32: false,
+					linux64: false,
+					mac_icns: './gmail_src/icon.icns',
+					zip: true,
+					credits: './gmail_src/credits.html'
+				},
+				src: ['./gmail_src/**/*']
+			},
+			google_apps: {
+				options: {
+					build_dir: './webkitbuilds',
+					mac: true,
+					win: false,
+					linux32: false,
+					linux64: false,
+					mac_icns: './google_apps_src/icon.icns',
+					zip: true,
+					credits: './google_apps_src/credits.html'
+				},
+				src: ['./google_apps_src/**/*']
+			},
+			fbm: {
+				options: {
+					build_dir: './webkitbuilds',
+					mac: true,
+					win: false,
+					linux32: false,
+					linux64: false,
+					mac_icns: './fbm_src/icon.icns',
+					zip: true,
+					credits: './fbm_src/credits.html'
+				},
+				src: ['./fbm_src/**/*']
+			},
+			wrike: {
+				options: {
+					build_dir: './webkitbuilds',
+					mac: true,
+					win: false,
+					linux32: false,
+					linux64: false,
+					mac_icns: './wrike_src/icon.icns',
+					zip: true,
+					credits: './wrike_src/credits.html'
+				},
+				src: ['./wrike_src/**/*']
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-node-webkit-builder');
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
 
-  grunt.registerTask('default', ['nodewebkit']);
+	grunt.registerTask('default', ['nodewebkit']);
 };
